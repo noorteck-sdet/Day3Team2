76dbdd3fcc0d3106827a8c58107f2e0911219e50
zalkh <zalkh@10.0.0.55>
2024-01-08 18:53:46 -0600

    new comment

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -3,6 +3,8 @@ package demo1;
 public class DemoTest1 {
 public static void main(String[] args) {
 		System.out.println("hELLO tEAM"); //yahiya
+		
+		System.out.println("Hi Yahya"); // Zain
 	}
 	
 }
