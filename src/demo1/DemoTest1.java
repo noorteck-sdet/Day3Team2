c16b415945c3b0e7857b94f3e76b593f435d0e99
amant <amant@Hadeel.lan>
2024-01-08 20:14:15 -0500

    hello from hadool

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -4,20 +4,16 @@ public class DemoTest1 {
 public static void main(String[] args) {
 		System.out.println("hELLO tEAM"); //yahiya
 		
+
 		System.out.println("Hi Yahya"); // Zain
 		
 		//happy birthday Yahiya -__-
 		System.out.println("This is a test...");
 		System.out.println("Kazim...."); 
 		
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/Day3Team2.git
-	}
-	public static void main(String [] args) {
-		System.out.println("Day 3 GitHub "); // Kazim.....
-		
-		//thank you Abass O_o
-		//Abass was here
+	
+		System.out.println("Java is Fun");    // Hadeel
 		
 	}
-
+	
 }
