812a809b1ecb0a9acae31939cc74c3029ad846e2
haithemalsaffar <155380799+haithemalsaffar@users.noreply.github.com>
2024-01-11 09:31:20 -0500

    Test

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -1,23 +1,21 @@
 package demo1;
 
 public class DemoTest1 {
-public static void main(String[] args) {
-		System.out.println("hELLO tEAM"); //yahiya
-		
+	public static void main(String[] args) {
+		System.out.println("hELLO tEAM"); // yahiya
 
 		System.out.println("Hi Yahya"); // Zain
-		
-		//happy birthday Yahiya -__-
+
+		// happy birthday Yahiya -__-
 		System.out.println("This is a test...");
-		System.out.println("Kazim...."); 
-		
-	
-		System.out.println("Java is Fun");    // Hadeel
-		System.out.println("Haithem"); // Haithem 
+		System.out.println("Kazim....");
+
+		System.out.println("Java is Fun"); // Hadeel
+		System.out.println("Haithem"); // Haithem
 		System.out.println("Pushing new code");// Haithem
-		System.out.println("Haithem"); // haithem 
+		System.out.println("Haithem"); // haithem
 		System.out.println("-----------------------------");
-		
+
 	}
-	
+
 }
